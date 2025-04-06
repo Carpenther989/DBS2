984f8100bb9bb68a248e600f1cfd5b1fb70e0940
Daniel Mrva <petr.kokos13@seznam.cz>
2025-04-06 12:21:34 +0200

    Carpenther first comit

--- index.php
+++ index.php
@@ -1,3 +1,122 @@
-<?php
-echo"tvoje mama";
-?>
+<!DOCTYPE html>
+<html lang="cs" data-bs-theme="dark">
+  <head>
+      <meta charset="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Tarkov Wiki</title>
+      <link rel="stylesheet" href="css/main.css"/>
+      <link rel="stylesheet" href="css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="fonts/6.0/css/all.min.css"/>
+
+      <script src="js/cookieManager.js"></script>
+  </head>
+  <body>
+  <div class="modal fade" id="modal_addAnime" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h1 class="modal-title fs-5">Správa anime</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                  <form class="row g-3" id="anime_form">
+                      <div class="col-md-12">
+                          <label for="input_name" class="form-label">Název</label>
+                          <input type="text" class="form-control" id="input_name" name="name">
+                      </div>
+                      <div class="col-md-6">
+                          <label for="input_seasons" class="form-label">Počet sezón</label>
+                          <input type="number" class="form-control" id="input_seasons" name="seasons">
+                      </div>
+                      <div class="col-6">
+                          <label for="input_episodes" class="form-label">Počet epizod per sezónu</label>
+                          <input type="number" class="form-control" id="input_episodes" name="episodes">
+                      </div>
+                      <div class="col-12">
+                          <label for="input_description">Popis</label>
+                          <textarea class="form-control" name="description" id="input_description"></textarea>
+                      </div>
+                      <div class="col-12">
+                          <button type="submit" class="btn btn-outline-primary">Přidat anime</button>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </div>
+      <header>
+          <nav class="header-top">
+              <div id="brand">
+                  <button type="button" id="logo" class="btn btn-light" data-dl-toggle>
+                      <i class="fa fa-sun"></i>
+                  </button>
+
+              </div>
+              <div id="menu">
+                  <ul>
+                      <li><a href="index.php">Home</a></li>
+                      <li id="add-anime" class="visually-hidden">
+                          <button type="button" data-bs-toggle="modal" data-bs-target="#modal_addAnime" >
+                              Přidat anime
+                          </button>
+                      </li>
+                      <li><a href="#my_anime_list">Questy</a></li>
+                      <li><a href="login.php" data-login>Přihlásit se</a></li>
+                  </ul>
+              </div>
+          </nav>
+      </header>
+
+      <div class="hero" id="section00">
+          <div class="overlay"></div>
+          <div class="hero-content">
+              <h1>Tarkov Wiki - Vaše Tarkovská wikipedie...</h1>
+              <!--<?php  echo "RANDOM SHIT"; ?> -->
+              <p>Lorem ipsum in dolor center</p>
+          </div>
+      </div>
+    <div class="clearfix"><br></div>
+    <div class="container">
+        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Questy</li>
+            </ol>
+        </nav>
+        <div class="clearfix"><br></div>
+        <div class="align-content-center">
+            <div class="mb-3">
+                <label for="search" class="form-label">Vyhledávání</label>
+                <input type="text" class="form-control" id="search" placeholder="Zadejte název questu">
+            </div>
+        </div>
+        <div class="clearfix"><br></div>
+        <div class="row" id="my_anime_list"></div>
+        <div class="clearfix"><br></div>
+    </div>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function ()
+            {
+                loadAnimeFromStorage();
+                if (getCookie('userName') !== null)
+                {
+                    updateMenu();
+                    buttonVisibility();
+                }
+            }
+        );
+
+        document.getElementById('search').addEventListener('keyup', function()
+            {
+                filterAnime(this.value);
+            }
+        );
+    </script>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/userRecognition.js"></script>
+    <script src="js/documentListener.js"></script>
+    <script src="js/formListener.js"></script>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Přihlášení</title>
+        <link href="css/login.css" rel="stylesheet">
+
+        <script src="js/cookieManager.js"></script>
+        <script src="js/userRecognition.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="text">
+                <a href="index.html" style="text-decoration: none"><small>Zpátky domů</small></a>
+            </div>
+            <form id="login_form" action="registration.php" method="POST">
+                <div class="form-row">
+                    <div class="input-data">
+                        <input type="text" required id="name"  autocomplete="new-password">
+                        <div class="underline"></div>
+                        <label for="name">Email</label>
+                    </div>
+
+                    
+                    <div class="input-data">
+                        <input type="password" required id="password"  autocomplete="new-password">
+                        <div class="underline"></div>
+                        <label for="password">Heslo</label>
+                    </div>
+                </div>
+                <div class="form-row submit-btn">
+                    <div class="input-data">
+                        <div class="inner"></div>
+                        <input type="submit" value="Přihlásit se">
+                    </div>
+                </div>
+                    
+            </form>
+
+            <script>
+                document.addEventListener("DOMContentLoaded", function ()
+                    {
+                        const form = document.getElementById('login_form');
+
+                        form.addEventListener('submit', (e) =>
+                            {
+                                e.preventDefault();
+                                checkExistingUser(form.querySelector('input[id=name]').value, form.querySelector('input[id=password]').value).then(result =>
+                                    {
+                                        if(result)
+                                        {
+                                            location.href = "index.html";
+                                        } else {
+                                            alert('Bad password or name')
+                                        }
+                                    }
+                                );
+
+                            }
+                        );
+                    }
+                );
+            </script>
+        </div>
+    </body>
+</html>
