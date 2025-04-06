287043ea10e6bee30869288cbaa8bde76a4215e2
kek <ne@ne.ne>
2025-04-06 17:19:17 +0200

    už vytahuju hash z databáze podle username

--- login-script.php
+++ login-script.php
@@ -1,9 +1,29 @@
 <?php
+//------------------------------------
+$host = "localhost";
+$user = "frantaVomacka";
+$password = "kekw";
+$dbname = "tarkov_wiki";
+try {
+    $conn = new pdo("mysql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo ":)";
+}
+catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+//---------------------------------
 
 
-$name = $_POST['name'];
-$password =  $_POST['password'];
-$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+
+//$name = $_POST['name'];
+//$password =  $_POST['password'];
+$stmt = $conn->prepare("SELECT getHashByName(:name)");
+$stmt->execute(['name' => 'testUser']);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+$firstKey = array_key_first($result);
+$result = $result[$firstKey];
+echo $result;
 
 //https://stackoverflow.com/questions/20556773/php-display-image-blob-from-mysql
 ?>
